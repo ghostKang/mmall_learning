5d44d7a648b6ca4daa9630ffd519e2766bf1d49d
ghostKang <672261493@qq.com>
2018-02-24 14:53:53 +0800

    支付宝当面付接ådemo

---
+++ src/main/java/com/mmall/controller/portal/OrderController.java
@@ -0,0 +1,14 @@
+package com.mmall.controller.portal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 订单模块
+ * Created by Yuk on 2018/2/23.
+ */
+@Controller
+@RequestMapping("/order/")
+public class OrderController {
+
+}
